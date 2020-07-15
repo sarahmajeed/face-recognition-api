167cb924aeb5f111a91e308a53a8880ac52cf227
Sarahmdawood <sarahmajeed368@gmail.com>
2020-07-15 13:41:52 +0500

    Setting up a dummy database and sigin/register routes

--- server.js
+++ server.js
@@ -2,8 +2,54 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
+const database = {
+  users: [
+    {
+      id: 123,
+      name: "John",
+      email: "John@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 124,
+      name: "Sally",
+      email: "Sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
 app.get('/', (req, res) => {
-  res.send('Yayyy! I am working')
+  res.send(database.users)
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('Success');
+  }
+  else {
+    res.status(400).json('Fail')
+  }
+
+})
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body
+  database.users.push({
+    id: 125,
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users);
 })
 
 app.listen(5000, () => console.log('Listening to port 5000'));

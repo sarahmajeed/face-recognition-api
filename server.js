f7b75ab8bbd4c7674bba53bc378d71b446ce4155
Sarahmdawood <sarahmajeed368@gmail.com>
2020-07-29 17:09:52 +0500

    Setting all routes and connecting to the database

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const saltRounds = 10;
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'software',
+    database: 'smartbrain'
+  }
+});
+
+console.log(db.select('id').from('users').then(data => {
+  console.log(data);
+}));
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -22,6 +41,14 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: "John@gmail.com"
+    }
+
   ]
 }
 
@@ -30,6 +57,14 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+  // Load hash from your password DB.
+  // bcrypt.compare('software', '$2b$10$tEUoRi8//vPORBwVluPryuen1na55qmlTQpTFTJyGOaLtIHBQUlTu', function (err, result) {
+  //   console.log('first guess', result);
+  // });
+  // bcrypt.compare('hello', '$2b$10$tEUoRi8//vPORBwVluPryuen1na55qmlTQpTFTJyGOaLtIHBQUlTu', function (err, result) {
+  //   console.log('second guess', result);
+  // });
+  console.log(req.body);
   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json('Success');
   }
@@ -39,38 +74,44 @@ app.post('/signin', (req, res) => {
 
 })
 
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body
-  database.users.push({
-    id: 125,
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users);
+app.post('/signup', (req, res) => {
+  const { name, email } = req.body
+  // bcrypt.hash(password, saltRounds).then(function (hash) {
+  //   console.log(hash);
+  // });
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => res.json(user[0]))
+    .catch(err => console.log(err.message))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
-  if (found === false) {
-    res.status(400).json('not found')
-  }
+  db
+    .select('*')
+    .from('users')
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json('not found')
+      }
+
+    }).catch(err => res.status(400).json('error getting user'))
 })
+
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
-    if (user.id === id) {
+    if (id === `${user.id}`) {
       found = true;
       user.entries++;
       return res.json(user.entries);
@@ -81,12 +122,12 @@ app.put('/image', (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('Listening to port 5000'));
+app.listen(5000, () => console.log('Listening to port 5000'))
 
 /*
   / --> res = this is working
   /signin --> POST = success/fail
-  /register --> POST = return added user
+  /signup --> POST = return added user
   /profile/:userid --> GET = user
   /image --> PUT --> Updated user object, count
 */

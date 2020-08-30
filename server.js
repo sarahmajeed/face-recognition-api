0242b0a6801dcb4e3aaad1169dcc47672049a2e9
Sarahmdawood <sarahmajeed368@gmail.com>
2020-08-30 14:59:40 +0500

    adding db

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "software",
-    database: "smartbrain",
+    host: "process.env.DATABASE_URL",
+    ssl: true,
   },
 });
 

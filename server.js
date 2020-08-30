073c8c6997ed1c8c95c94028ce271911ea1aff72
Sarahmdawood <sarahmajeed368@gmail.com>
2020-08-30 14:04:37 +0500

    Adding bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const saltRounds = 10;
 const knex = require("knex");

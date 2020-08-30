30dd420599ae72c03e78ce46606a13a9f31b954f
Sarahmdawood <sarahmajeed368@gmail.com>
2020-08-30 15:01:20 +0500

    conn string

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "process.env.DATABASE_URL",
+    connectionString: "process.env.DATABASE_URL",
     ssl: true,
   },
 });

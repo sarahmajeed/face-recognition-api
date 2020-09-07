da74d802f63e77d6827948f5e294a3609290fa8f
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-07 12:47:14 +0500

    disable node_modules cache

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: "process.env.DATABASE_URL",
-    ssl: true,
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "software",
+    database: "smartbrain",
   },
 });
 
